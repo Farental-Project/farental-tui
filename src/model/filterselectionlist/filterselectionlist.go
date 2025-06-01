189ec93b730529b8e20a798069fe41b8d9752f56
Halsten <leeser.lionel@gmail.com>
2025-06-01 14:08:13 +0200

    Generized the filterselectionlist

---
+++ src/model/filterselectionlist/filterselectionlist.go
@@ -0,0 +1,179 @@
+package filterselectionlist
+
+import (
+	"farental/internal/config"
+	"farental/internal/context"
+	"farental/internal/keybind"
+	"farental/model"
+	"farental/style"
+	"github.com/charmbracelet/bubbles/help"
+	"github.com/charmbracelet/bubbles/key"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+	"github.com/charmbracelet/lipgloss"
+	"strings"
+)
+
+type Model struct {
+	List   list.Model
+	Items  []list.Item
+	Help   help.Model
+	Keymap config.ModularKeyMap
+
+	Title string
+
+	loadData func() []list.Item
+	submit   func(m *Model) bool
+}
+
+func New(title string, listItemDelegate list.ItemDelegate, loadData func() []list.Item, submit func(m *Model) bool) Model {
+	m := Model{}
+
+	m.Help = help.New()
+
+	m.Items = make([]list.Item, 0)
+
+	m.List = list.New(m.Items,
+		listItemDelegate,
+		style.LayoutWidth, 45)
+	m.List.SetShowHelp(false)
+	m.List.DisableQuitKeybindings()
+
+	m.Title = title
+	m.loadData = loadData
+	m.submit = submit
+
+	m.Keymap = config.ModularKeyMap{}
+
+	m.updateKeymap()
+
+	return m
+}
+
+func (m Model) Init() tea.Cmd {
+	return model.InitCmd
+}
+
+func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case model.InitMsg:
+		m.UpdateData()
+
+		return m, nil
+
+	case tea.KeyMsg:
+		switch {
+		case key.Matches(msg, keybind.Quit):
+			return m, tea.Quit
+
+		case key.Matches(msg, keybind.Submit):
+			if m.List.FilterState() != list.Filtering {
+				ok := m.submit(&m)
+
+				if ok {
+					return context.ContentManager.SwitchContent(
+						model.ContentGameDashboard)
+				}
+
+				return m, nil
+			}
+
+		case key.Matches(msg, keybind.HelpMore):
+			m.Help.ShowAll = !m.Help.ShowAll
+
+		case key.Matches(msg, keybind.Back):
+			if m.List.FilterState() == list.Unfiltered {
+				return context.ContentManager.SwitchContent(
+					model.ContentGameDashboard)
+			}
+		}
+	}
+
+	context.ContentManager.Update(msg)
+
+	m.List, cmd = m.List.Update(msg)
+
+	m.updateKeymap()
+
+	return m, cmd
+}
+
+func (m Model) View() string {
+	var tui strings.Builder
+
+	helpText := m.Help.View(m.Keymap)
+
+	title := style.TitleStyle.Render(m.Title)
+
+	tui.WriteString(title)
+	tui.WriteString("\n\n")
+	tui.WriteString(style.ContainerStyle.Width(style.LayoutWidth).Render(m.List.View()))
+	tui.WriteString("\n\n")
+	tui.WriteString(helpText)
+
+	return lipgloss.Place(
+		context.ContentManager.ScreenWidth,
+		context.ContentManager.ScreenHeight,
+		lipgloss.Center, lipgloss.Center,
+		tui.String())
+}
+
+func (m *Model) UpdateData() {
+	m.Items = m.loadData()
+	m.List.SetItems(m.Items)
+}
+
+func (m *Model) updateKeymap() {
+	var fullKeys [][]key.Binding
+	var leftColumn []key.Binding
+	var rightColumn []key.Binding
+	var essentialsKeys []key.Binding
+	var escKey key.Binding
+	var enterKey key.Binding
+
+	keybind.HelpMore.SetEnabled(true)
+
+	switch m.List.FilterState() {
+	case list.Filtering:
+		escKey = keybind.Cancel
+		enterKey = keybind.Apply
+	case list.FilterApplied:
+		escKey = keybind.ClearFilter
+		enterKey = keybind.Submit
+	case list.Unfiltered:
+		escKey = keybind.Back
+		enterKey = keybind.Submit
+	}
+
+	if m.List.FilterState() == list.Filtering {
+		essentialsKeys = append(essentialsKeys, enterKey, escKey)
+		keybind.HelpMore.SetEnabled(false)
+		m.Keymap.SetEssentialBindings(essentialsKeys)
+		return
+	}
+
+	essentialsKeys = append(essentialsKeys,
+		keybind.Up, keybind.Down, keybind.Filter, escKey, enterKey, keybind.HelpMore)
+
+	leftColumn = append(leftColumn,
+		keybind.Up,
+		keybind.Down,
+		keybind.GotoListStart,
+		keybind.GotoListEnd,
+	)
+
+	rightColumn = append(rightColumn,
+		keybind.Filter,
+		keybind.Submit,
+		keybind.Back,
+		keybind.Quit,
+		keybind.HelpClose,
+	)
+
+	fullKeys = append(fullKeys, leftColumn, rightColumn)
+
+	m.Keymap.SetBindings(fullKeys)
+	m.Keymap.SetEssentialBindings(essentialsKeys)
+}
--- src/model/travelselection/travelselection.go
+++ src/model/travelselection/travelselection.go
@@ -3,129 +3,59 @@ package travelselection
 import (
 	"farental/core/data/api"
 	"farental/core/request"
-	"farental/internal/config"
-	"farental/internal/context"
-	"farental/internal/keybind"
 	"farental/internal/lang"
-	"farental/model"
-	"farental/style"
-	"github.com/charmbracelet/bubbles/help"
-	"github.com/charmbracelet/bubbles/key"
+	"farental/model/filterselectionlist"
 	"github.com/charmbracelet/bubbles/list"
 	tea "github.com/charmbracelet/bubbletea"
-	"github.com/charmbracelet/lipgloss"
 	"log"
-	"strings"
 )
 
 type Model struct {
-	List   list.Model
-	Items  []list.Item
-	Help   help.Model
-	Keymap config.ModularKeyMap
-
-	Title string
+	FilterSelectionList filterselectionlist.Model
 }
 
 func New() Model {
 	m := Model{}
 
-	m.Help = help.New()
-
-	m.Items = make([]list.Item, 0)
-
-	m.List = list.New(m.Items,
+	m.FilterSelectionList = filterselectionlist.New(
+		lang.L("Travel selection"),
 		ListItemDelegate{},
-		style.LayoutWidth, 45)
-	m.List.SetShowHelp(false)
-	m.List.DisableQuitKeybindings()
-
-	m.Title = lang.L("Travel selection")
-
-	m.Keymap = config.ModularKeyMap{}
-
-	m.updateKeymap()
+		m.loadData,
+		m.submit)
 
 	return m
 }
 
 func (m Model) Init() tea.Cmd {
-	return model.InitCmd
+	return m.FilterSelectionList.Init()
 }
 
 func (m Model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	var cmd tea.Cmd
+	var mod tea.Model
 
-	switch msg := msg.(type) {
-	case model.InitMsg:
-		m.UpdateData()
+	mod, cmd = m.FilterSelectionList.Update(msg)
 
-		return m, nil
+	modFSL, ok := mod.(filterselectionlist.Model)
 
-	case tea.KeyMsg:
-		switch {
-		case key.Matches(msg, keybind.Quit):
-			return m, tea.Quit
-
-		case key.Matches(msg, keybind.Submit):
-			if m.List.FilterState() != list.Filtering {
-				ok := m.submit()
-
-				if ok {
-					return context.ContentManager.SwitchContent(
-						model.ContentGameDashboard)
-				}
-
-				return m, nil
-			}
-
-		case key.Matches(msg, keybind.HelpMore):
-			m.Help.ShowAll = !m.Help.ShowAll
-
-		case key.Matches(msg, keybind.Back):
-			if m.List.FilterState() == list.Unfiltered {
-				return context.ContentManager.SwitchContent(
-					model.ContentGameDashboard)
-			}
-		}
+	if !ok {
+		return mod, cmd
 	}
 
-	context.ContentManager.Update(msg)
-
-	m.List, cmd = m.List.Update(msg)
-
-	m.updateKeymap()
+	m.FilterSelectionList = modFSL
 
 	return m, cmd
 }
 
 func (m Model) View() string {
-	var tui strings.Builder
-
-	helpText := m.Help.View(m.Keymap)
-
-	title := style.TitleStyle.Render(m.Title)
-
-	tui.WriteString(title)
-	tui.WriteString("\n\n")
-	tui.WriteString(style.ContainerStyle.Width(style.LayoutWidth).Render(m.List.View()))
-	tui.WriteString("\n\n")
-	tui.WriteString(helpText)
-
-	return lipgloss.Place(
-		context.ContentManager.ScreenWidth,
-		context.ContentManager.ScreenHeight,
-		lipgloss.Center, lipgloss.Center,
-		tui.String())
+	return m.FilterSelectionList.View()
 }
 
-func (m *Model) UpdateData() {
-	m.loadTravels()
-	m.List.SetItems(m.Items)
-}
-
-func (m *Model) loadTravels() {
+func (m *Model) loadData() []list.Item {
 	var travels []api.TravelResponse
+	var items []list.Item
+
+	items = make([]list.Item, 0)
 
 	req := request.TravelGetAvailable()
 
@@ -133,24 +63,24 @@ func (m *Model) loadTravels() {
 
 	if err != nil {
 		log.Println(err)
-		return
+		return items
 	}
 
 	travels = *resp.Result().(*[]api.TravelResponse)
-
-	m.Items = make([]list.Item, 0)
 
 	for _, t := range travels {
 		item := ListItem{
 			Travel: t,
 		}
 
-		m.Items = append(m.Items, item)
+		items = append(items, item)
 	}
+
+	return items
 }
 
-func (m *Model) submit() bool {
-	i, ok := m.List.SelectedItem().(ListItem)
+func (m *Model) submit(fsl *filterselectionlist.Model) bool {
+	i, ok := fsl.List.SelectedItem().(ListItem)
 
 	if !ok {
 		return false
@@ -171,57 +101,4 @@ func (m *Model) submit() bool {
 	}
 
 	return true
-}
-
-func (m *Model) updateKeymap() {
-	var fullKeys [][]key.Binding
-	var leftColumn []key.Binding
-	var rightColumn []key.Binding
-	var essentialsKeys []key.Binding
-	var escKey key.Binding
-	var enterKey key.Binding
-
-	keybind.HelpMore.SetEnabled(true)
-
-	switch m.List.FilterState() {
-	case list.Filtering:
-		escKey = keybind.Cancel
-		enterKey = keybind.Apply
-	case list.FilterApplied:
-		escKey = keybind.ClearFilter
-		enterKey = keybind.Submit
-	case list.Unfiltered:
-		escKey = keybind.Back
-		enterKey = keybind.Submit
-	}
-
-	if m.List.FilterState() == list.Filtering {
-		essentialsKeys = append(essentialsKeys, enterKey, escKey)
-		keybind.HelpMore.SetEnabled(false)
-		m.Keymap.SetEssentialBindings(essentialsKeys)
-		return
-	}
-
-	essentialsKeys = append(essentialsKeys,
-		keybind.Up, keybind.Down, keybind.Filter, escKey, enterKey, keybind.HelpMore)
-
-	leftColumn = append(leftColumn,
-		keybind.Up,
-		keybind.Down,
-		keybind.GotoListStart,
-		keybind.GotoListEnd,
-	)
-
-	rightColumn = append(rightColumn,
-		keybind.Filter,
-		keybind.Submit,
-		keybind.Back,
-		keybind.Quit,
-		keybind.HelpClose,
-	)
-
-	fullKeys = append(fullKeys, leftColumn, rightColumn)
-
-	m.Keymap.SetBindings(fullKeys)
-	m.Keymap.SetEssentialBindings(essentialsKeys)
 }
