eddc53342c623ac44e5bb489f25ca4bcc6a948d1
Halsten <leeser.lionel@gmail.com>
2025-08-03 06:53:37 +0200

    Added filterablelist widget

---
+++ src/widget/filterablelist/filterablelist.go
@@ -0,0 +1,59 @@
+package filterablelist
+
+import (
+	"farental/internal/orvyn"
+	"github.com/charmbracelet/bubbles/list"
+	tea "github.com/charmbracelet/bubbletea"
+)
+
+type Widget struct {
+	orvyn.BaseWidget
+
+	list.Model
+
+	MinSize       orvyn.Size
+	PreferredSize orvyn.Size
+}
+
+func New(delegate list.ItemDelegate, items []list.Item) *Widget {
+	w := new(Widget)
+
+	w.BaseWidget = orvyn.NewBaseWidget()
+
+	w.Model = list.New(items, delegate, 0, 0)
+	w.Model.DisableQuitKeybindings()
+
+	w.Model.SetShowStatusBar(false)
+	w.Model.SetShowHelp(false)
+	w.Model.SetShowTitle(false)
+	w.Model.SetShowPagination(true)
+
+	return w
+}
+
+func (w *Widget) Update(msg tea.Msg) tea.Cmd {
+	var cmd tea.Cmd
+
+	w.Model, cmd = w.Model.Update(msg)
+
+	return cmd
+}
+
+func (w *Widget) Render() string {
+	return w.Model.View()
+}
+
+func (w *Widget) Resize(size orvyn.Size) {
+	w.BaseWidget.Resize(size)
+
+	w.Model.SetWidth(size.Width)
+	w.Model.SetHeight(size.Height)
+}
+
+func (w *Widget) GetMinSize() orvyn.Size {
+	return w.MinSize
+}
+
+func (w *Widget) GetPreferredSize() orvyn.Size {
+	return w.PreferredSize
+}
