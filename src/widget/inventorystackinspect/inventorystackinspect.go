0c446c349b9ae0b153c70f2ee3d90e1d5622879e
Halsten <leeser.lionel@gmail.com>
2025-08-06 22:41:29 +0200

    Creation of the inventorystackinspect widget for the inventory (Not tested yet)
    I need to create the inventory screen next

---
+++ src/widget/inventorystackinspect/inventorystackinspect.go
@@ -0,0 +1,173 @@
+package inventorystackinspect
+
+import (
+	"farental/core/data/api"
+	"farental/internal/lang"
+	"farental/internal/orvyn"
+	"farental/internal/orvyn/layout"
+	"farental/style"
+	"fmt"
+	"strings"
+)
+
+type Widget struct {
+	orvyn.BaseWidget
+
+	srName            *orvyn.SimpleRenderable
+	srUnique          *orvyn.SimpleRenderable
+	srDescription     *orvyn.SimpleRenderable
+	srStatsTitle      *orvyn.SimpleRenderable
+	srStats           *orvyn.SimpleRenderable
+	srConditionsTitle *orvyn.SimpleRenderable
+	srConditions      *orvyn.SimpleRenderable
+	srResultsTitle    *orvyn.SimpleRenderable
+	srResults         *orvyn.SimpleRenderable
+
+	layout *layout.VBoxLayout
+
+	currentStackItemID uint
+}
+
+func New() *Widget {
+	w := new(Widget)
+
+	w.BaseWidget = orvyn.NewBaseWidget()
+
+	titleStyle := style.DimBottomBorderStyle
+
+	w.srName = orvyn.NewSimpleRenderable("")
+	w.srUnique = orvyn.NewSimpleRenderable(lang.L("Unique"))
+	w.srUnique.Style = style.SpecialHighlightStyle
+	w.srUnique.SetActive(false)
+	w.srDescription = orvyn.NewSimpleRenderable("")
+
+	w.srStatsTitle = orvyn.NewSimpleRenderable(lang.L("Stats"))
+	w.srStatsTitle.Style = titleStyle
+	w.srStats = orvyn.NewSimpleRenderable("")
+	w.srStatsTitle.SetActive(false)
+	w.srStats.SetActive(false)
+
+	w.srConditionsTitle = orvyn.NewSimpleRenderable(lang.L("Equip conditions"))
+	w.srConditionsTitle.Style = titleStyle
+	w.srConditions = orvyn.NewSimpleRenderable("")
+	w.srConditionsTitle.SetActive(false)
+	w.srConditions.SetActive(false)
+
+	w.srResultsTitle = orvyn.NewSimpleRenderable("Effects")
+	w.srResultsTitle.Style = titleStyle
+	w.srResults = orvyn.NewSimpleRenderable("")
+	w.srResultsTitle.SetActive(false)
+	w.srResults.SetActive(false)
+
+	w.layout = layout.NewMaxWidthVBoxLayout(1,
+		[]orvyn.Renderable{
+			w.srName,
+			w.srUnique,
+			w.srDescription,
+			w.srStatsTitle,
+			w.srStats,
+			w.srConditionsTitle,
+			w.srConditions,
+			w.srResultsTitle,
+			w.srResults,
+		},
+	)
+
+	return w
+}
+
+func (w *Widget) Render() string {
+	return w.layout.Render()
+}
+
+func (w *Widget) Resize(size orvyn.Size) {
+	w.BaseWidget.Resize(size)
+
+	w.layout.Resize(size)
+}
+
+func (w *Widget) UpdateData(stack *api.StackResponse) {
+	var b strings.Builder
+	var show bool
+
+	w.currentStackItemID = stack.ItemID
+
+	w.srName.SetValue(stack.Item.Name)
+	w.srDescription.SetValue(stack.Item.Description)
+	w.srUnique.SetActive(stack.Item.IsUnique)
+
+	isEquipment := stack.Item.EquipmentSlot != nil
+
+	if isEquipment {
+		// Stats
+		for i, s := range *stack.Item.EquipmentStats {
+			if i > 0 {
+				b.WriteString("\n")
+			}
+
+			b.WriteString(fmt.Sprintf("• %s : %d", s.Stat.Name, s.Value))
+		}
+
+		show = b.Len() > 0
+
+		w.srStatsTitle.SetActive(show)
+		w.srStats.SetActive(show)
+
+		if show {
+			w.srStats.SetValue(b.String())
+			b.Reset()
+		}
+
+		// Conditions
+		for i, c := range *stack.Item.Conditions {
+			if i > 0 {
+				b.WriteString("\n")
+			}
+
+			b.WriteString(fmt.Sprintf("• %s", c))
+		}
+
+		show = b.Len() > 0
+
+		w.srConditionsTitle.SetActive(show)
+		w.srConditions.SetActive(show)
+
+		if show {
+			w.srConditions.SetValue(b.String())
+			b.Reset()
+		}
+	} else {
+		w.srStatsTitle.SetActive(false)
+		w.srStats.SetActive(false)
+		w.srConditionsTitle.SetActive(false)
+		w.srConditions.SetActive(false)
+	}
+
+	isUseable := stack.Item.Results != nil
+
+	if isUseable {
+		for i, r := range *stack.Item.Results {
+			if i > 0 {
+				b.WriteString("\n")
+			}
+
+			b.WriteString(fmt.Sprintf("• %s", r))
+		}
+
+		show = b.Len() > 0
+
+		w.srResultsTitle.SetActive(show)
+		w.srResults.SetActive(show)
+
+		if show {
+			w.srResults.SetValue(b.String())
+		}
+	} else {
+		w.srResultsTitle.SetActive(false)
+		w.srResults.SetActive(false)
+	}
+}
+
+func (w *Widget) GetCurrentStackItemID() uint {
+	return w.currentStackItemID
+}
