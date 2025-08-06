0cd9dc43e2a5764e4df8eb72838eb4d497bb8779
Halsten <leeser.lionel@gmail.com>
2025-08-06 22:19:09 +0200

    Added style management to the simpleRenderable in orvyn

--- src/internal/orvyn/simple.go
+++ src/internal/orvyn/simple.go
@@ -1,26 +1,41 @@
 package orvyn
 
+import (
+	"github.com/charmbracelet/lipgloss"
+)
+
 type SimpleRenderable struct {
 	BaseRenderable
 
-	view string
+	Style lipgloss.Style
+
+	value string
 }
 
 var VGap = NewSimpleRenderable("\n")
 
-func NewSimpleRenderable(view string) *SimpleRenderable {
+func NewSimpleRenderable(value string) *SimpleRenderable {
 	s := new(SimpleRenderable)
 
 	s.BaseRenderable = NewBaseRenderable()
-	s.view = view
+	s.value = value
+	s.Style = lipgloss.NewStyle()
 
 	return s
 }
 
-func (s *SimpleRenderable) SetView(view string) {
-	s.view = view
+func (s *SimpleRenderable) SetValue(value string) {
+	s.value = value
 }
 
 func (s *SimpleRenderable) Render() string {
-	return s.view
+	return s.Style.Render(s.value)
+}
+
+func (s *SimpleRenderable) GetMinSize() Size {
+	return GetRenderSize(s.Style, s.value)
+}
+
+func (s *SimpleRenderable) GetPreferredSize() Size {
+	return GetRenderSize(s.Style, s.value)
 }
